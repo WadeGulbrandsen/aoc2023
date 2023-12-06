b97d0c88959ba681be6c6ab09daee95b48d65ea2
Wade Gulbrandsen <wade.gulbrandsen@gmail.com>
2023-12-05 23:27:16 -0500

    Prep for Day 06

---
+++ cmd/day06/day06.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/WadeGulbrandsen/aoc2023/internal"
+
+func Problem1(filename string) int {
+	defer internal.Un(internal.Trace("Problem1"))
+	return 0
+}
+
+func Problem2(filename string) int {
+	defer internal.Un(internal.Trace("Problem2"))
+	return 0
+}
