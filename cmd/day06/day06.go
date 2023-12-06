b34e270d27e8563ba9954c241c426125a167e9cb
Wade Gulbrandsen <wade.gulbrandsen@gmail.com>
2023-12-05 23:30:59 -0500

    Added main() function

--- cmd/day06/day06.go
+++ cmd/day06/day06.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/WadeGulbrandsen/aoc2023/internal"
+import (
+	"fmt"
+
+	"github.com/WadeGulbrandsen/aoc2023/internal"
+)
 
 func Problem1(filename string) int {
 	defer internal.Un(internal.Trace("Problem1"))
@@ -10,4 +14,11 @@ func Problem1(filename string) int {
 func Problem2(filename string) int {
 	defer internal.Un(internal.Trace("Problem2"))
 	return 0
+}
+
+func main() {
+	filename := "input.txt"
+	fmt.Println("Advent of Code 2023")
+	fmt.Printf("\nThe answer for Day 06, Problem 1 is: %v\n\n", Problem1(filename))
+	fmt.Printf("\nThe answer for Day 06, Problem 2 is: %v\n\n", Problem2(filename))
 }
