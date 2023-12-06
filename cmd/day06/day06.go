a6bd4e049040f271f14507eb70c197a4f4c7fda4
Wade Gulbrandsen <wade.gulbrandsen@gmail.com>
2023-12-06 02:31:21 -0500

    Day 06 done

--- cmd/day06/day06.go
+++ cmd/day06/day06.go
@@ -1,18 +1,131 @@
+// tt: total time
+// th: time holding the button
+// tm: time moving
+// dt: distance traveled
+//
+// equation 1: th + tm = tt
+//             th = tt - tm
+//
+// equation 2: th * tm = dt
+//             th = dt/tm
+//
+// combined:   tt - tm = dt/tm
+//             (tt - tm) * tm = dt
+//             (tt*tm) - (tm*tm) = dt
+//             (tt*tm) = (tm*tm) + dt
+//             0 = (tm*tm) - (tt*tm) + dt
+//
+// Quadratic equation!
+// a = 1
+// b = -tt
+// c = dt
+
 package main
 
 import (
 	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
 
 	"github.com/WadeGulbrandsen/aoc2023/internal"
 )
 
+func Quadratic(a, b, c float64) (float64, float64, error) {
+	discriminant := (b * b) - (4 * a * c)
+	rooted := math.Sqrt(discriminant)
+	if math.IsNaN(rooted) {
+		return math.NaN(), math.NaN(), fmt.Errorf("discriinant is less than zero: %v^2 - 4(%v)(%v) = %v", b, a, c, discriminant)
+	}
+	x1 := (-b + rooted) / (2 * a)
+	x2 := (-b - rooted) / (2 * a)
+	return x1, x2, nil
+}
+
+type Race struct {
+	time, distance int
+}
+
+func getIntsFromString(s string) []int {
+	var ints []int
+	for _, x := range strings.Split(s, " ") {
+		if v, err := strconv.Atoi(strings.TrimSpace(x)); err == nil {
+			ints = append(ints, v)
+		}
+	}
+	return ints
+}
+
+func getRacesFromFile(filename string) []Race {
+	var r []Race
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	times, distances, found := strings.Cut(string(data), "\n")
+	if !found {
+		panic(fmt.Errorf("couldn't split data into lines"))
+	}
+	if t, d := getIntsFromString(times), getIntsFromString(distances); t != nil && d != nil && len(t) == len(d) {
+		for i, time := range t {
+			r = append(r, Race{time, d[i]})
+		}
+	}
+	return r
+}
+
 func Problem1(filename string) int {
 	defer internal.Un(internal.Trace("Problem1"))
-	return 0
+	races := getRacesFromFile(filename)
+	var results []int
+	for _, r := range races {
+		if x1, x2, err := Quadratic(1, -float64(r.time), float64(r.distance)); err == nil {
+			h := math.Ceil(max(x1, x2)) - 1
+			l := math.Floor(min(x1, x2)) + 1
+			diff := h - l
+			results = append(results, int(diff)+1)
+		}
+	}
+	product := 1
+	for _, r := range results {
+		product *= r
+	}
+	return product
+}
+
+func onlyDigits(s string) int {
+	keepDigits := func(r rune) rune {
+		if unicode.IsDigit(r) {
+			return r
+		}
+		return -1
+	}
+	digits := strings.Map(keepDigits, s)
+	if v, err := strconv.Atoi(strings.TrimSpace(digits)); err == nil {
+		return v
+	}
+	return -1
 }
 
 func Problem2(filename string) int {
 	defer internal.Un(internal.Trace("Problem2"))
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	times, distances, found := strings.Cut(string(data), "\n")
+	if !found {
+		panic(fmt.Errorf("couldn't split data into lines"))
+	}
+	b, c := onlyDigits(times), onlyDigits(distances)
+	if x1, x2, err := Quadratic(1, -float64(b), float64(c)); err == nil {
+		h := math.Ceil(max(x1, x2)) - 1
+		l := math.Floor(min(x1, x2)) + 1
+		diff := h - l
+		return int(diff) + 1
+	}
 	return 0
 }
 
--- cmd/day06/day06_test.go
+++ cmd/day06/day06_test.go
@@ -7,22 +7,22 @@ import (
 
 func TestSolutions(t *testing.T) {
 	t.Run("Problem1 with sample.txt", func(t *testing.T) {
-		if a, r := 35, Problem1("sample.txt"); a != r {
+		if a, r := 288, Problem1("sample.txt"); a != r {
 			t.Fatalf("The correct answer is %v but received %v", a, r)
 		}
 	})
 	t.Run("Problem1 with input.txt", func(t *testing.T) {
-		if a, r := 157211394, Problem1("input.txt"); a != r {
+		if a, r := 4403592, Problem1("input.txt"); a != r {
 			t.Fatalf("The correct answer is %v but received %v", a, r)
 		}
 	})
 	t.Run("Problem2 with sample.txt", func(t *testing.T) {
-		if a, r := 46, Problem2("sample.txt"); a != r {
+		if a, r := 71503, Problem2("sample.txt"); a != r {
 			t.Fatalf("The correct answer is %v but received %v", a, r)
 		}
 	})
 	t.Run("Problem2 with input.txt", func(t *testing.T) {
-		if a, r := 50855035, Problem2("input.txt"); a != r {
+		if a, r := 38017587, Problem2("input.txt"); a != r {
 			t.Fatalf("The correct answer is %v but received %v", a, r)
 		}
 	})
