26ea075b511b5ad8195f52957ae1810a6f5076b9
Wade Gulbrandsen <wade.gulbrandsen@gmail.com>
2023-12-06 16:25:36 -0500

    Refactored tests and made a template

---
+++ cmd/template/day0.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+const Day = 0
+
+func Problem1(filename string) int {
+	return 0
+}
+
+func Problem2(filename string) int {
+	return 0
+}
+
+func main() {
+	filename := "input.txt"
+	fmt.Println("Advent of Code 2023")
+	fmt.Printf("\nThe answer for Day %v, Problem 1 is: %v\n\n", Day, Problem1(filename))
+	fmt.Printf("\nThe answer for Day %v, Problem 2 is: %v\n\n", Day, Problem2(filename))
+}
--- cmd/template/day0_test.go
+++ cmd/template/day0_test.go
@@ -7,30 +7,28 @@ import (
 )
 
 // Answers
-const p1sample = 288
-const p1input = 4403592
-const p2sample = 71503
-const p2input = 38017587
-
-const day = 6
+const p1sample = 0
+const p1input = 0
+const p2sample = 0
+const p2input = 0
 
 func TestSolutions(t *testing.T) {
-	t.Run(fmt.Sprintf("Day %v Problem1 with sample.txt", day), func(t *testing.T) {
+	t.Run(fmt.Sprintf("Day %v Problem1 with sample.txt", Day), func(t *testing.T) {
 		if a, r := p1sample, Problem1("sample.txt"); a != r {
 			t.Fatalf("The correct answer is %v but received %v", a, r)
 		}
 	})
-	t.Run(fmt.Sprintf("Day %v Problem1 with input.txt", day), func(t *testing.T) {
+	t.Run(fmt.Sprintf("Day %v Problem1 with input.txt", Day), func(t *testing.T) {
 		if a, r := p1input, Problem1("input.txt"); a != r {
 			t.Fatalf("The correct answer is %v but received %v", a, r)
 		}
 	})
-	t.Run(fmt.Sprintf("Day %v Problem2 with sample.txt", day), func(t *testing.T) {
+	t.Run(fmt.Sprintf("Day %v Problem2 with sample.txt", Day), func(t *testing.T) {
 		if a, r := p2sample, Problem2("sample.txt"); a != r {
 			t.Fatalf("The correct answer is %v but received %v", a, r)
 		}
 	})
-	t.Run(fmt.Sprintf("Day %v Problem2 with input.txt", day), func(t *testing.T) {
+	t.Run(fmt.Sprintf("Day %v Problem2 with input.txt", Day), func(t *testing.T) {
 		if a, r := p2input, Problem2("input.txt"); a != r {
 			t.Fatalf("The correct answer is %v but received %v", a, r)
 		}
