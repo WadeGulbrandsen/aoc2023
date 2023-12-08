a0dd15439a1cffbfdb09a9df2a68e28c945fc422
Wade Gulbrandsen <wade.gulbrandsen@gmail.com>
2023-12-08 03:18:33 -0500

    Fixed typo

--- internal/math.go
+++ internal/math.go
@@ -20,7 +20,7 @@ func Quadratic(a, b, c float64) (float64, float64, error) {
 	discriminant := (b * b) - (4 * a * c)
 	rooted := math.Sqrt(discriminant)
 	if math.IsNaN(rooted) {
-		return math.NaN(), math.NaN(), fmt.Errorf("discriinant is less than zero: %v^2 - 4(%v)(%v) = %v", b, a, c, discriminant)
+		return math.NaN(), math.NaN(), fmt.Errorf("discriminant is less than zero: %v^2 - 4(%v)(%v) = %v", b, a, c, discriminant)
 	}
 	x1 := (-b + rooted) / (2 * a)
 	x2 := (-b - rooted) / (2 * a)
